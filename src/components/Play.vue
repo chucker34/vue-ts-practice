b7163da829d5c4ffc67a5c0558ac2e2d6470af0d
chucker34 <kawakami34@gmail.com>
2019-05-21 18:26:11 +0900

    適当に入力値を表示してみた

---
+++ src/components/Play.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="play">
+    <h1>{{ displayMsg }}</h1>
+    <input v-model="inputMsg">
+    <button @click="update">更新</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Play extends Vue {
+    @Prop() private propsMsg!: string;
+
+    inputMsg: string = '';
+
+    displayMsg: string = this.propsMsg
+
+    update () {
+        this.displayMsg = this.propsMsg + this.inputMsg
+        this.inputMsg = ''
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Play propsMsg="ここでいろいろ遊んでいく"/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Play from '@/components/Play.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    Play,
+  },
+})
+export default class Abount extends Vue {}
+</script>
